08562dd1b332fe92339cf9155a39d7add9bc45b8
fmattaperdomo <fmattaperdomo@gmail.com>
2024-12-08 09:00:31 -0500

    Create component sales-person-list

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-person-list',
+  imports: [],
+  templateUrl: './sales-person-list.component.html',
+  styleUrl: './sales-person-list.component.css'
+})
+export class SalesPersonListComponent {
+
+}
